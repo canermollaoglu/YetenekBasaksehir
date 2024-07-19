efa1ccb3e0b11f1f8ce21454a9e5d11b309c9a7d
Hamza Dursun <hmzdrsn64@gmail.com>
2024-07-19 12:09:47 +0300

    Yeni Buton Eklendi

--- WinFormsApp6/Form1.cs
+++ WinFormsApp6/Form1.cs
@@ -27,5 +27,10 @@ namespace WinFormsApp6
         {
             MessageBox.Show("Test");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
